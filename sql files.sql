c0dcc83b0f2dac42a3ef616f23b264cbac886d20
morganaoliveira <morganaferreira2009@hotmail.com>
2025-11-10 12:02:59 -0300

    Adiciona script SQL e descrição do projeto e-commerce

---
+++ sql files.sql
@@ -0,0 +1,15 @@
+-- funções e cláusulas de agrupamento 
+USE company_constraints;
+show databases;
+show tables;
+ select Fname, Salary, Dno from employee;
+update employee set Salary =
+	case
+		when Dno=5 then Salary+ 2000
+        when Dno=4 then Salary+ 1500
+        when Dno=1 then Salary+ 3000
+        else Salary + 0
+	end;
+    
+   
+    
